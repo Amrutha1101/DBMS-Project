0aa913f7f7d8c3207afb0849ce5ca0b6417fd93b
ujwal <ujju@gamil.com>
2022-01-17 20:09:57 +0530

    added a view_booked file and updated user_page

--- user_page.php
+++ user_page.php
@@ -2,6 +2,7 @@
 
 echo " <a href=\"reserve.php\">Reserve Ticket</a><br>";
 echo " <a href=\"cancel_ticket.html\">Cancel Ticket</a><br>";
+echo " <a href=\"view_booked.php\">View Tickets</a><br>";
 echo " <a href=\"logout.php\">Logout</a><br>";
 
 ?>
